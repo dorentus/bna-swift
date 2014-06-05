57c15d062a697e643cdf17fc0fc28da7666f77e8
Zhang Yi <zhangyi.cn@gmail.com>
2014-06-05 13:49:49 +0800

    Initial Commit

---
+++ bna/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bna
+//
+//  Created by Rox Dorentus on 14-6-5.
+//  Copyright (c) 2014年 rubyist.today. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
