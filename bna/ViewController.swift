1f2c5806235b3189e52fa103388b730abe783883
Zhang Yi <zhangyi.cn@gmail.com>
2014-06-13 14:11:15 +0800

    cleanup

--- bna/ViewController.swift
+++ bna/ViewController.swift
@@ -14,31 +14,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-        let serial = "CN-1402-1943-1283"
-        let secret = "4202aa2182640745d8a807e0fe7e34b30c1edb23"
-        let restorecode = "4CKBN08QEB"
-
-        let sl = Serial(serial)
-        let st = Secret(secret)
-
-        let r0 = Restorecode(sl, st)
-        println(r0.text)
-
-        let r1 = Restorecode(restorecode)
-        println(r1.text)
-
-        let r2 = Restorecode(serial, secret)
-        println(r2.text)
-
-        println(hmac_sha1_hexdigest(serial, secret))
-        println(hmac_sha1_hexdigest(serial.bytes, secret.bytes))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
